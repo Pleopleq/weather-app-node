1f2994d9a3e444cc975d9e8172b813c87d3b775d
Felix Anducho <felix.anducho74@gmail.com>
2020-11-30 16:37:08 -0600

    Finish console weather app/Node

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 require('dotenv').config()
-const request = require("request")
+const geoCode = require("./utils/geocode")
+const weather = require("./utils/weather")
+const address = process.argv[2]
 
-const weather = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=37.8267,-122.4233`
-const geocode = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.GEOCODE_API_KEY}&limit=1`
-
-function options(url) {
-    const parameters ={
-            url: url,
-            json: true
-    }
-    return parameters
+if (!address) {
+    console.log("Please provide a search address")
+} else {
+    geoCode(address, (error, latLong) => {
+        if (error) {
+            return console.log(error)
+        }
+        weather(latLong.latitude, latLong.longitude, (error, weather) => {
+            if (error) {
+                return console.log(error)
+            }
+            
+            console.log(latLong.location)
+            console.log(weather)
+        }) 
+    })
+    
 }
 
-request(options(weather), (error, response, body) => {
-    console.log(`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
-})
 
-request(options(geocode), (error, response, body) => {
-    const latitude = body.features[0].center[1]
-    const longitude = body.features[0].center[0]
-    console.log(latitude, longitude)
-})
+
---
+++ utils/geocode.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const request = require("request")
+
+function geoCode(address, callback) {
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.GEOCODE_API_KEY}&limit=1`
+    const parameters ={
+        url: url,
+        json: true
+    }
+
+    request(parameters, (error, response, body) => {
+        if (error) {
+            callback("Unable to connect to location services", undefined)
+        } else if (body.features.length === 0) {
+            callback("Unable to find location. Try another search", undefined)
+        } else {
+            const latitude = body.features[0].center[1]
+            const longitude = body.features[0].center[0]
+            const location = body.features[0].place_name
+
+            callback(undefined, {
+                latitude: latitude,
+                longitude: longitude,
+                location: location
+            })
+        }
+    })
+}
+
+module.exports = geoCode
